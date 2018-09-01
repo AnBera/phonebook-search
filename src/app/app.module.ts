b9ef5e618bd83bf6f970ef440f4d50279be6317f
Anirban Bera <anirbanb@Anirbans-MacBook-Air.local>
2018-09-01 14:48:49 +0530

    set up angular  material
    ref url
    https://coursetro.com/posts/code/113/How-to-Build-an-Angular-5-Material-App

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 
 import { AppComponent } from './app.component';
+import { ContactCardComponent } from './contact-card/contact-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
