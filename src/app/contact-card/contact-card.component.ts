7f711b4494a75825bd2857af77aad193656460e1
Anirban Bera <anirbanb@Anirbans-MacBook-Air.local>
2018-09-01 20:26:25 +0530

    Basic working card with repeat

--- src/app/contact-card/contact-card.component.ts
+++ src/app/contact-card/contact-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ContactCard } from '../model/class/contact-card';
 
 @Component({
   selector: 'app-contact-card',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-card.component.scss']
 })
 export class ContactCardComponent implements OnInit {
+  contactCards: ContactCard[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    this.contactCards = [new ContactCard('Anirban', 9883223453),
+                        new ContactCard('Murali', 9862635475)];
   }
 
 }
---
+++ src/app/model/class/contact-card.ts
@@ -0,0 +1,27 @@
+import { ICard } from '../interface/ICard';
+
+export class ContactCard implements ICard {
+    name;
+    contactNo;
+
+    constructor (name, contactNo) {
+        this.name = name;
+        this.contactNo = contactNo;        
+    }
+
+    get Name() {
+        return this.name;
+    }
+
+    set Name(name) {
+        this.name = name;
+    }
+
+    get ContactNo() {
+        return this.contactNo;
+    }
+
+    set ContactNo(contactNo) {
+        this.contactNo = contactNo;
+    }
+}
