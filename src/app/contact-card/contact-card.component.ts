ed1826e8650fbf62c12fb25f9107a155515e972d
Anirban Bera <anirbanb@Anirbans-MacBook-Air.local>
2018-09-02 13:43:20 +0530

    random color generator

--- src/app/contact-card/contact-card.component.ts
+++ src/app/contact-card/contact-card.component.ts
@@ -10,12 +10,14 @@ import { ContactsService } from '../service/contacts.service';
 export class ContactCardComponent implements OnInit {
   contactCards: ContactCard[] = [];
   query: string;
+  private colorsMap: object = {};
 
   constructor(private contactsService: ContactsService) { }
 
   ngOnInit() {
     // this.contactCards = [new ContactCard('Anirban', 9883223453),
     //                     new ContactCard('Murali', 9862635475)];
+    this.populateRandomColor();
     this.getCards();
     this.contactsService.currentSearch.subscribe(query => this.query = query);
   }
@@ -23,6 +25,34 @@ export class ContactCardComponent implements OnInit {
   getCards(): void {
     this.contactsService.getContacts()
     .subscribe(contactCards => this.contactCards = contactCards);
+  }
+
+  populateRandomColor(): void {
+    var letters = '012345'.split('');
+    var color = '#'; 
+    var alphabet = "abcdefghijklmnopqrstuvwxyz".split('');       
+    color += letters[Math.round(Math.random() * 5)];
+    letters = '0123456789ABCDEF'.split('');
+    for (let i = 0; i < alphabet.length; i++){
+      for (let j = 0; j < 6; j++) {
+        color += letters[Math.round(Math.random() * 15)];
+      }
+      this.colorsMap[alphabet[i]] = color;
+      color = '#';
+    }    
+    console.log(this.colorsMap);
+  } 
+
+  public setStyles(card): any {
+    // var styles;
+    if (card.name){
+      return {            
+        'background-color': this.colorsMap[card.name[0].toLowerCase()]
+      }
+    }
+         
+    // return styles;
+    // {'background-color' : colorsMap[card.name[0].toString()] }
   }
 
 }
